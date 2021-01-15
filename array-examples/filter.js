6ac2d7ffab09b982dd4a4880ea6c8acd8f72265d
elmasurgen <71708089+elmasurgen@users.noreply.github.com>
2021-01-15 18:34:00 +0300

    add filter example

---
+++ array-examples/filter.js
@@ -0,0 +1,15 @@
+
+const items = [
+    { name: "Bisiklet", price: 100 },
+    { name: "TV", price: 2000 },
+    { name: "Kitap", price: 50 },
+    { name: "Telefon", price: 3000 },
+    { name: "Bilgisayar", price: 6000 },
+    { name: "Klavye", price: 200 }
+  ];
+  
+  const filteredItems = items.filter(item => {
+    return item.price <= 200;
+  });
+  
+  console.log(filteredItems);
