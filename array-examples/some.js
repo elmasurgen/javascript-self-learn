ce85952e97a2eb0ea0094606586f9767280695ce
elmasurgen <71708089+elmasurgen@users.noreply.github.com>
2021-01-16 00:58:21 +0300

    some

---
+++ array-examples/some.js
@@ -0,0 +1,14 @@
+const items = [
+    { name: "Bisiklet", price: 100 },
+    { name: "TV", price: 2000 },
+    { name: "Kitap", price: 50 },
+    { name: "Telefon", price: 3000 },
+    { name: "Bilgisayar", price: 6000 },
+    { name: "Klavye", price: 200 }
+  ];
+  
+  const hasInexpensiveItems = items.some(item => {
+    return item.price <=100
+  });
+  
+  console.log(hasInexpensiveItems);
